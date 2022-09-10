7240252c53dac6d1bc38072c6142d0fd3772cf6a
aragorn_x64 <advaysagarkar@gmail.com>
2022-09-11 01:02:28 +0530

    feat(server): working backend

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # amazon-product-scraper
  - Simple JS based web-scraper to extract useful information from an Amazon product page
- 
- - Currently only a server side app to display certain essential information in the console
+<!--  
+ - Currently only a server side app to display certain essential information in the console -->
+ - Currently runs in the backend, but has to still be connected to a frontend
+ - Can scrape some essential information from a Amazon product page ( use links of the type http://localhost:8080/scrape/?scrapeUrl=https://amzn.eu/d/1ZGGxKT for now)
  
  To do: 
- - Make an API and thus use the scraped data in a webpage
+ [-] Make an API and thus use the scraped data in a webpage
+ [ ] Connect API to frontend to display the product data
--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 import fetch from 'node-fetch';
 import * as cheerio from 'cheerio';
+import express from 'express';
+import cors from 'cors';
 
-const urltpc = 'https://amzn.eu/d/5SZAHrl';
-let links = ['https://amzn.eu/d/5SZAHrl', 'https://www.amazon.in/dp/B09SPNKT16/ref=syn_sd_onsite_desktop_92?ie=UTF8&psc=1&pd_rd_plhdr=t&tag=local0ea-21'];
+const PORT = 8080;
+const app = express();
+const router = express.Router();
+app.use("/", router);
+app.use(express.json());
+
+app.use(cors('localhost'));
+  
+app.listen( PORT, () => {
+    console.log(`App is running at http://localhost:${PORT}`);
+})
+
+app.get('/scrape', async function (req, res){
+    console.log("got request");
+
+    let quer = req.query;
+    let link = quer['scrapeUrl'];
+    let prod = await getProduct(link);
+    console.log(prod);
+
+    res.status(200).send(prod);
+})
+
+async function getFromLink(link){
+    console.log('reached getFromLink');
+    let prod1 = await getProduct(link);
+    console.log(prod1);
+    return prod1;
+}
+
 
 async function getProduct(url) {
     try {
@@ -20,9 +50,9 @@ async function getProduct(url) {
             price: price
         };
 
-        console.log(prod);
+        // console.log(prod);
 
-        // return prod;
+        return prod;
 
 
     } catch (error) {
@@ -32,4 +62,7 @@ async function getProduct(url) {
 }
 
 // getProduct(urltpc);
-links.forEach(getProduct);
+// links.forEach(getProduct);
+// const urltpc = 'https://amzn.eu/d/5SZAHrl';
+// let links = ['https://amzn.eu/d/5SZAHrl', 'https://www.amazon.in/dp/B09SPNKT16/ref=syn_sd_onsite_desktop_92?ie=UTF8&psc=1&pd_rd_plhdr=t&tag=local0ea-21'];
+
